009bdb8e94dbd0770b13858485322778381f9f70
Eugen Soloviov <suenot@gmail.com>
2015-06-07 12:22:30 +0300

    build

--- assets/js/vendor/owl-carousel/owl.property.js
+++ assets/js/vendor/owl-carousel/owl.property.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   $("#slider-product-material").owlCarousel({
-      //autoPlay: 3000, //Set AutoPlay to 3 seconds
+      autoPlay: 5000, //Set AutoPlay to 3 seconds
       items : 1,
       theme : "theme-product-material",
+      singleItem : true
   });
 });
